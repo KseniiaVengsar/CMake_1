b67ce1cd3de8df6d439012fdcf4abb6c6c899510
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2023-05-04 19:00:42 +0100

    first commit

---
+++ Source1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+	setlocale(LC_ALL, "ru");
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте,  "<<name<<"!";
+	return 0;
+}
